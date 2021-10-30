118692104b06e1eb4cab6e5e8a7ebf953f4cdab6
Frank Stein <dr.frank.stain@gmail.com>
2021-10-30 09:57:31 +0300

    Public headers implemented.

---
+++ include/.public/black/open-gl.full.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../.prologue.h"
+
+// Hosted subsystems.
+#include "../../es-3.0/es-3.0.h"
+
+// The epilogue of library.
+#include "../../.epilogue.h"
---
+++ include/.public/black/open-gl/es-3.0.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../../.prologue.h"
+
+// OpenGL ES 3.0 bindings.
+#include "../../../es-3.0/es-3.0.h"
+
+// The epilogue of library.
+#include "../../../.epilogue.h"

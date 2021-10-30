5e548bafe00c84fdb9f166350ccc67fd980fa0a8
Frank Stein <dr.frank.stain@gmail.com>
2021-10-30 10:11:20 +0300

    First code for main headers of Windows desktop platform.

---
+++ include/platform.windows/platform.windows.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+// External dependencies.
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+// Core Windows desktop platform definitions.
+inline namespace WindowsDesktop
+{
+
+}
+}
+}
+}

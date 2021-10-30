5b86e2bca726668fef1ce02dc3ad9196aa91f8af
Frank Stein <dr.frank.stain@gmail.com>
2021-10-30 11:46:40 +0300

    First code for OpenGL ES 3.0 core API subsystem.

---
+++ include/es-3.0/es-3.0.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+// Fundamental external dependencies.
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+// OpenGL ES 3.0 core interface subsystem.
+inline namespace GlEs30
+{
+// Internal namespace for GL ES 3.0 subsystem.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Most fundamental definitions.
+
+// Generated headers.

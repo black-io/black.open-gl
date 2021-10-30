d9df06c60f4c07822e49b0f76a814583308490c7
Frank Stein <dr.frank.stain@gmail.com>
2021-10-30 09:54:07 +0300

    Library namespace defined.

---
+++ include/.bootstrap/namespace.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+// Black Framework main namespace.
+namespace Black
+{
+// OpenGL bindings library.
+inline namespace Core
+{
+// Internal layer of OpenGL Library.
+namespace Internal
+{
+
+}
+}
+}

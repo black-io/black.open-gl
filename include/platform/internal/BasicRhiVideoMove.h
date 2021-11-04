15c996c0fd60266bf5c6f74e6fc0e02b2727dbaa
Frank Stein <dr.frank.stain@gmail.com>
2021-11-04 14:01:23 +0300

    Basic implementation of RHI for display video mode.

---
+++ include/platform/internal/BasicRhiVideoMove.h
@@ -0,0 +1,74 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+namespace Internal
+{
+	/**
+		@brief	Basic implementation of display video mode.
+
+		RHI for display video mode should implement the interface for graphics display to configure the identified video mode.
+
+		This type implements the first layer of platform-agnostic RHI video mode type.
+		This type is strictly internal and should never been used outside of subsystem.
+	*/
+	class BasicRhiVideoMode : private Black::NonTransferable
+	{
+	// Heirs construction and initialization.
+	protected:
+		BasicRhiVideoMode()								= default;
+		BasicRhiVideoMode( const BasicRhiVideoMode& )	= default;
+		~BasicRhiVideoMode()							= default;
+
+	// Public interface.
+	public:
+		// Get the width of resolution.
+		inline const size32_t GetWidth() const			{ return m_width; };
+
+		// Get the height of resolution.
+		inline const size32_t GetHeight() const			{ return m_height; };
+
+		// Get the bit rate.
+		inline const size32_t GetBitRate() const		{ return m_bit_rate; };
+
+		// Get the refresh rate.
+		inline const size32_t GetRefreshRate() const	{ return m_refresh_rate; };
+
+		// Get the aspect ratio, which represents `width / height` proportion.
+		inline const size32_t GetAspectRatio() const	{ return m_aspect; };
+
+
+		// Whether the video mode may be used as full-screen display mode.
+		inline const bool HasFullscreenSupport() const	{ return m_allow_fullscreen; };
+
+		// Whether the video mode may be used for window while display in windowed mode.
+		inline const bool HasWindowSupport() const		{ return m_allow_windowed; };
+
+	// Heirs state.
+	protected:
+		size32_t	m_width			= 0;	// Width in pixels.
+		size32_t	m_height		= 0;	// Height in pixels.
+		size32_t	m_bit_rate		= 0;	// Color depth in bits.
+		size32_t	m_refresh_rate	= 0;	// Frequency of vertical synchronization in Hz.
+		float		m_aspect		= 0.0f;	// Aspect of display area.
+
+		union
+		{
+			uint32_t	m_boolean_bits		= 0;	// Blob for boolean values.
+
+			struct
+			{
+				bool	m_allow_fullscreen	: 1;	// Whether the video mode may be used as full-screen display mode.
+				bool	m_allow_windowed	: 1;	// Whether the video mode may be used for window while display in windowed mode.
+			};
+		};
+	};
+}
+}
+}
+}

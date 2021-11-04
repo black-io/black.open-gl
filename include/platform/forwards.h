53e674ffba191730a1d1a25deab7397a3542ac86
Frank Stein <dr.frank.stain@gmail.com>
2021-11-04 10:52:32 +0300

    Some platform-related types forwarded to allow it usage in platform-specific code.

---
+++ include/platform/forwards.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+	// Forward the RHI adapter to be used in platform-specific code.
+	class RhiAdapter;
+
+	// Forward the RHI display to be used in platform-specific code.
+	class RhiDisplay;
+
+	// Forward the configuration to be used in platform-specific code.
+	class GlConfiguration;
+
+	// Forward the context to be used in platform-specific code.
+	class GlContext;
+}
+}
+}

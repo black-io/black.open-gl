1cef72bdf493b5514aa6491731a9e1e3d7783f41
Frank Stein <dr.frank.stain@gmail.com>
2021-11-04 17:54:46 +0300

    Basic RHI implementation for GPU connection.

---
+++ include/platform/internal/BasicRhiConnection.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+namespace Internal
+{
+	/**
+		@brief	Basic implementation of GPU connection.
+
+		RHI connection should implement the functionality to communicate with current hardware setup.
+		All other RHI types may be used only with support of RHI connection.
+
+		This type implements the first layer of platform-agnostic RHI connection type.
+		This type is strictly internal and should never been used outside of subsystem.
+	*/
+	class BasicRhiConnection : private Black::NonTransferable
+	{
+	// Heirs construction and initialization.
+	protected:
+		BasicRhiConnection()							= default;
+		BasicRhiConnection( const BasicRhiConnection& )	= default;
+		~BasicRhiConnection()							= default;
+	};
+}
+}
+}
+}

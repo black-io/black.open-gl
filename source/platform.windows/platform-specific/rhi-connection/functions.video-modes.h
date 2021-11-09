51d4efdb88c64def431a2d1832797ec322bf648b
Frank Stein <dr.frank.stain@gmail.com>
2021-11-09 17:07:54 +0300

    Code style fixed.

--- source/platform.windows/platform-specific/rhi-connection/functions.video-modes.h
+++ source/platform.windows/platform-specific/rhi-connection/functions.video-modes.h
@@ -10,15 +10,13 @@ inline namespace Platform
 namespace PlatformSpecific
 {
 	// Suitable formats of video mode.
-	inline constexpr ::DXGI_FORMAT VIDEO_MODE_FORMATS[]
-	{
+	inline constexpr ::DXGI_FORMAT VIDEO_MODE_FORMATS[] {
 		DXGI_FORMAT_R10G10B10A2_UNORM, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_FORMAT_B8G8R8X8_UNORM,
 		DXGI_FORMAT_B4G4R4A4_UNORM, DXGI_FORMAT_B5G5R5A1_UNORM, DXGI_FORMAT_B5G6R5_UNORM
 	};
 
 	// Association of bit-rates for suitable formats of video mode.
-	inline constexpr size32_t VIDEO_MODE_BIT_RATES[]
-	{
+	inline constexpr size32_t VIDEO_MODE_BIT_RATES[] {
 		32, 32, 32, 32,
 		16, 16, 16
 	};

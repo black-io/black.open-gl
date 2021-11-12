f95861a7aa3597656ae87a6346fed1dc8fb14726
Frank Stein <dr.frank.stain@gmail.com>
2021-11-12 14:18:28 +0300

    Implementation of EGL-compatible display object in terms of Windows desktop platform.

---
+++ include/platform.windows/platform-specific/EglDisplay.windows.h
@@ -0,0 +1,103 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+namespace PlatformSpecific
+{
+	/**
+		@brief	Platform dependent EGL-compatible display implementation.
+
+		Implements the second layer of platform-agnostic EGL display type.
+		This type is strictly internal and should never been used outside of subsystem.
+	*/
+	template<>
+	class EglDisplay<Black::PlatformType::WindowsDesktop> : public Internal::BasicEglDisplay
+	{
+	// Platform-specific interface.
+	public:
+		// Perform the display connection to default display of given adapter.
+		const bool Connect( const Black::GlRhiAdapter& adapter, ::IDXGIFactory& factory );
+
+		// Perform the display connection to given display.
+		const bool Connect( const Black::GlRhiDisplay& display, ::IDXGIFactory& factory );
+
+
+		// Query the interface of bound GPU adapter. May return `nullptr` if display is not connected.
+		inline ::IDXGIAdapter* QueryAdapterInterface() const			{ return m_adapter_interface.get(); };
+
+		// Query the interface of bound GPU adapter. May return `nullptr` if display is not connected.
+		inline ::IDXGIAdapter1* QueryAdapterV1Interface() const			{ return m_adapter_v1_interface.get(); };
+
+		// Query the interface of bound display. May return `nullptr` if display is not connected.
+		inline ::IDXGIOutput* QueryOutputInterface() const				{ return m_output_interface.get(); };
+
+		// Get the instance of device context. May return `nullptr` if display is not connected.
+		inline const ::HDC GetDeviceContext() const						{ return m_device_context; };
+
+		// Get the description of display device.
+		inline const ::DXGI_OUTPUT_DESC& GetOutputDescription() const	{ return m_device_desc; };
+
+		// Get the default video mode of display.
+		inline const ::DXGI_MODE_DESC& GetDefaultVideoMode() const		{ return m_default_mode; };
+
+		// Get the additional information about display monitor.
+		inline const ::MONITORINFOEXW& GetDisplayMonitorInfo() const	{ return m_monitor_info; };
+
+		// Get the additional information about display.
+		inline const ::DISPLAY_DEVICEW& GetDisplayInfo() const			{ return m_display_info; };
+
+	// Platform-agnostic interface.
+	public:
+		// Finalize the device connection.
+		void Finalize();
+
+
+		// Whether the display is connected to device.
+		inline const bool IsConnected() const { return m_device_context != nullptr; };
+
+	// Heirs construction and initialization.
+	protected:
+		EglDisplay()	= default;
+		~EglDisplay();
+
+	// Private interface.
+	private:
+		// Acquire the interface of connected GPU.
+		const bool AcquireAdapterInterface( size32_t adapter_index, ::IDXGIFactory& factory );
+
+		// Acquire the interface of connected display.
+		const bool AcquireOutputInterface( size32_t output_index, ::IDXGIFactory& factory );
+
+		// Manually collect the display information.
+		const bool CollectDisplayInfo();
+
+		// Copy the display information from given handle.
+		const bool CopyDisplayInfo( const Black::GlRhiDisplay& display );
+
+		// Try to create the device context.
+		const bool CreateDeviceContext();
+
+		// Delete the owned device context.
+		void DeleteDeviceContext();
+
+	// Private state.
+	private:
+		Black::UniqueComPointer<::IDXGIAdapter>		m_adapter_interface;				// Owned interface of GPU device.
+		Black::UniqueComPointer<::IDXGIAdapter1>	m_adapter_v1_interface;				// Owned interface of GPU device.
+		Black::UniqueComPointer<::IDXGIOutput>		m_output_interface;					// Owned instance of display device.
+		::HDC										m_device_context		= nullptr;	// Owned device context of display.
+
+		::DXGI_OUTPUT_DESC	m_device_desc;		// Internal description of display device.
+		::DXGI_MODE_DESC	m_default_mode;		// Default video mode of display.
+		::MONITORINFOEXW	m_monitor_info;		// Additional information about display.
+		::DISPLAY_DEVICEW	m_display_info;		// Additional information about display.
+	};
+}
+}
+}
+}
---
+++ source/platform.windows/platform-specific/EglDisplay.windows.cpp
@@ -0,0 +1,158 @@
+#include <black/open-gl.h>
+
+#include "rhi-connection/functions.dxgi.h"
+#include "rhi-connection/functions.video-modes.h"
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+namespace PlatformSpecific
+{
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/OpenGL/EGL Display";
+}
+
+
+	const bool EglDisplay<Black::PlatformType::WindowsDesktop>::Connect( const Black::GlRhiAdapter& adapter, ::IDXGIFactory& factory )
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting to default display of adapter #{}.", adapter.GetIndex() );
+
+		CRETE( !AcquireAdapterInterface( adapter.GetIndex(), factory ), false, LOG_CHANNEL, "Failed to connect to default display of adapter." );
+		CRETE( !AcquireOutputInterface( 0, factory ), false, LOG_CHANNEL, "Failed to connect to default display of adapter." );
+		CRETE( !CollectDisplayInfo(), false, LOG_CHANNEL, "Failed to connect to default display of adapter." );
+		CRETE( !CreateDeviceContext(), false, LOG_CHANNEL, "Failed to connect to default display of adapter." );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Successfully connected to default display of adapter #{}.", adapter.GetIndex() );
+		return true;
+	}
+
+	const bool EglDisplay<Black::PlatformType::WindowsDesktop>::Connect( const Black::GlRhiDisplay& display, ::IDXGIFactory& factory )
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting to display #{} of adapter #{}.", display.GetIndex(), display.GetAdapterIndex() );
+
+		CRETE( !AcquireAdapterInterface( display.GetAdapterIndex(), factory ), false, LOG_CHANNEL, "Failed to connect to display." );
+		CRETE( !AcquireOutputInterface( display.GetIndex(), factory ), false, LOG_CHANNEL, "Failed to connect to display." );
+		CRETE( !CopyDisplayInfo( display ), false, LOG_CHANNEL, "Failed to connect to display." );
+		CRETE( !CreateDeviceContext(), false, LOG_CHANNEL, "Failed to connect to display." );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Successfully connected to display #{} of adapter #{}.", display.GetIndex(), display.GetAdapterIndex() );
+		return true;
+	}
+
+	void EglDisplay<Black::PlatformType::WindowsDesktop>::Finalize()
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Starting the display finalization." );
+
+		DeleteDeviceContext();
+		m_output_interface.reset();
+		m_adapter_v1_interface.reset();
+		m_adapter_interface.reset();
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Display finalized properly." );
+	}
+
+	EglDisplay<Black::PlatformType::WindowsDesktop>::~EglDisplay()
+	{
+		Finalize();
+	}
+
+	const bool EglDisplay<Black::PlatformType::WindowsDesktop>::AcquireAdapterInterface( size32_t adapter_index, ::IDXGIFactory& factory )
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Capturing the interface of adapter #{}.", adapter_index );
+
+		m_adapter_interface.reset( PlatformSpecific::QueryAdapterInterface( factory, adapter_index ) );
+		CRETE( m_adapter_interface == nullptr, false, LOG_CHANNEL, "Failed to capture interface of adapter #{}.", adapter_index );
+
+		m_adapter_v1_interface.reset( PlatformSpecific::QueryAdapterV1Interface( *m_adapter_interface ) );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Interface of adapter #{} captured successfully.", adapter_index );
+		return true;
+	}
+
+	const bool EglDisplay<Black::PlatformType::WindowsDesktop>::AcquireOutputInterface( size32_t output_index, ::IDXGIFactory& factory )
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Capturing the interface of display #{}.", output_index );
+
+		EXPECTS_DEBUG( m_adapter_interface != nullptr );
+
+		m_output_interface.reset( PlatformSpecific::QueryOutputInterface( *m_adapter_interface, output_index ) );
+		CRETE( m_output_interface == nullptr, false, LOG_CHANNEL, "Failed to capture interface of display #{}.", output_index );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Capturing the interface of display #{}.", output_index );
+		return true;
+	}
+
+	const bool EglDisplay<Black::PlatformType::WindowsDesktop>::CollectDisplayInfo()
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Collecting the display information." );
+
+		::HRESULT access_result = S_OK;
+
+		BLACK_LOG_VERBOSE( LOG_CHANNEL, "Reading the description of display." );
+		access_result = m_output_interface->GetDesc( &m_device_desc );
+		CRETE( FAILED( access_result ), false, LOG_CHANNEL, "Failed to get description of display, result: 0x{:08X}.", access_result );
+
+		BLACK_LOG_VERBOSE( LOG_CHANNEL, "Reading the monitor information for display." );
+		m_monitor_info.cbSize = size32_t( sizeof( m_monitor_info ) );
+		const bool has_monitor_info	= ::GetMonitorInfoW( m_device_desc.Monitor, &m_monitor_info ) == TRUE;
+		CRETE( !has_monitor_info, false, LOG_CHANNEL, "Failed to get the monitor information for display." );
+
+		BLACK_LOG_VERBOSE( LOG_CHANNEL, "Reading the device information for display." );
+		m_display_info.cb = size32_t( sizeof( m_display_info ) );
+		const bool has_display_info	= ::EnumDisplayDevicesW( m_device_desc.DeviceName, 0, &m_display_info, 0 ) == TRUE;
+		CRETE( !has_display_info, false, LOG_CHANNEL, "Failed to get device info for display." );
+
+		BLACK_LOG_VERBOSE( LOG_CHANNEL, "Detecting the default display mode." );
+		m_default_mode = FindDefaultDisplayMode( *m_output_interface, m_monitor_info );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Display information collected successfully." );
+		return true;
+	}
+
+	const bool EglDisplay<Black::PlatformType::WindowsDesktop>::CopyDisplayInfo( const Black::GlRhiDisplay& display )
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Copying the display information." );
+
+		Black::CopyMemory( m_device_desc, display.GetDeviceDescription() );
+		Black::CopyMemory( m_default_mode, display.GetDefaultVideoMode() );
+		Black::CopyMemory( m_monitor_info, display.GetDisplayMonitorInfo() );
+		Black::CopyMemory( m_display_info, display.GetDisplayInfo() );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Display information copied successfully." );
+		return true;
+	}
+
+	const bool EglDisplay<Black::PlatformType::WindowsDesktop>::CreateDeviceContext()
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Creating the device context for display." );
+
+		EXPECTS_DEBUG( m_device_context == nullptr );
+		CRETE( m_output_interface == nullptr, false, LOG_CHANNEL, "Failed to create device context for display." );
+
+		m_device_context = ::CreateDCW( nullptr, m_device_desc.DeviceName, nullptr, nullptr );
+		CRETE( m_device_context == nullptr, false, LOG_CHANNEL, "Failed to create temporary device context." );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Device context created successfully." );
+		return true;
+	}
+
+	void EglDisplay<Black::PlatformType::WindowsDesktop>::DeleteDeviceContext()
+	{
+		CRET( m_device_context == nullptr );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Device context of display will be deleted." );
+		if( ::DeleteDC( std::exchange( m_device_context, nullptr ) ) == FALSE )
+		{
+			BLACK_LOG_WARNING( LOG_CHANNEL, "Device context of display was not deleted properly. No errors provided." );
+		}
+	}
+}
+}
+}
+}

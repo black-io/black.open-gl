306c4c4c37ee7fffcd5805387d4302dc398a528b
Frank Stein <dr.frank.stain@gmail.com>
2021-11-12 17:44:47 +0300

    Basic EGL display now stores the list of EGL configurations.

--- include/platform/internal/BasicEglDisplay.h
+++ include/platform/internal/BasicEglDisplay.h
@@ -17,10 +17,19 @@ namespace Internal
 	*/
 	class BasicEglDisplay : private Black::NonTransferable
 	{
+	// Public interface.
+	public:
+		// Get the list of available EGL configurations for display.
+		inline Black::PlainVector<const Black::EglConfiguration> GetConfigurations() const { return { m_configurations.data(), m_configurations.size() }; };
+
 	// Heirs construction and initialization.
 	protected:
 		BasicEglDisplay()	= default;
 		~BasicEglDisplay()	= default;
+
+	// Heirs state.
+	protected:
+		std::vector<Black::EglConfiguration> m_configurations; // List of Available configurations for display.
 	};
 }
 }

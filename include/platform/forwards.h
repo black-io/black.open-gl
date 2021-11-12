9a771f32de94d26bfa3af7117e40fcc2b1ba8242
Frank Stein <dr.frank.stain@gmail.com>
2021-11-12 17:44:11 +0300

    Name of EGL configuration type changed.

--- include/platform/forwards.h
+++ include/platform/forwards.h
@@ -13,11 +13,11 @@ inline namespace Platform
 	// Forward the RHI display to be used in platform-specific code.
 	class GlRhiDisplay;
 
+	// Forward the EGL configuration to be used in platform-specific code.
+	class EglConfiguration;
+
 	// Forward the EGL display object to be used in platform-specific code.
 	class EglDisplay;
-
-	// Forward the configuration to be used in platform-specific code.
-	class GlConfiguration;
 
 	// Forward the context to be used in platform-specific code.
 	class GlContext;

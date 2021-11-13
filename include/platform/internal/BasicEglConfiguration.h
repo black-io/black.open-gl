3e22dddcb95517168b12422d683479cc46f679e1
Frank Stein <dr.frank.stain@gmail.com>
2021-11-13 13:12:43 +0300

    First code for basic EGL-compatible confiuration.

--- include/platform/internal/BasicEglConfiguration.h
+++ include/platform/internal/BasicEglConfiguration.h
@@ -12,11 +12,16 @@ namespace Internal
 	/**
 		@brief	Basic implementation of EGL-compatible configuration object.
 
-		This type implements the first layer of platform-agnostic GL configuration type.
+		This type implements the first layer of platform-agnostic EGL configuration type.
 		This type is strictly internal and should never been used outside of subsystem.
 	*/
-	class BasicGlConfiguration : private Black::NonTransferable
+	class BasicEglConfiguration : private Black::NonMovable
 	{
+	// Heirs construction and initialization.
+	protected:
+		BasicEglConfiguration()									= default;
+		BasicEglConfiguration( const BasicEglConfiguration& )	= default;
+		~BasicEglConfiguration()								= default;
 	};
 }
 }

46d292820f776e2108eb6484fb5101588242060e
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 01:03:46 +0300

    Pixel buffer settings type for pixel format.

---
+++ include/platform.windows/internal/structures.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+namespace Internal
+{
+	// Pixel format settings for pixel buffers.
+	struct PixelBufferSettings final
+	{
+		int32_t			maximum_width	= 0;	// Value of maximum width.
+		int32_t			maximum_height	= 0;	// Value of maximum height.
+		int32_t			maximum_pixels	= 0;	// Maximum number of pixels allowed for single pixel buffer.
+		int32_t			optimal_width	= 0;	// Value of optimal width.
+		int32_t			optimal_height	= 0;	// Value of optimal height.
+
+		union
+		{
+			int32_t		bits			= 0;	// Buffer to store the bits of boolean flags.
+
+			struct
+			{
+				bool	is_supported	: 1;	// Whether the pixel format supports the pixel buffers.
+			};
+		};
+	};
+}
+}
+}
+}

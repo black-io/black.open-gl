c6ddf952cc32c91a67f0394d908f690bdd459ef7
Frank Stein <dr.frank.stain@gmail.com>
2021-11-14 09:01:54 +0300

    Remove redundant function.

--- source/platform.windows/platform-specific/rhi-connection/functions.pixel-formats.h
+++ source/platform.windows/platform-specific/rhi-connection/functions.pixel-formats.h
@@ -14,32 +14,6 @@ namespace PlatformSpecific
 
 	// Sort the pixel formats.
 	std::vector<::PIXELFORMATDESCRIPTOR*> SortPixelFormats( Black::PlainView<::PIXELFORMATDESCRIPTOR> pixel_formats );
-
-	//
-	template< typename TFunction >
-	inline std::vector<::PIXELFORMATDESCRIPTOR*> FilterPixelFormats( std::vector<::PIXELFORMATDESCRIPTOR*>&& source_formats, TFunction function )
-	{
-		std::vector<::PIXELFORMATDESCRIPTOR*> result{ std::move( source_formats ) };
-
-		std::vector<::PIXELFORMATDESCRIPTOR*>::iterator write_head = result.begin();
-		for( std::vector<::PIXELFORMATDESCRIPTOR*>::const_iterator read_head = result.begin(); read_head != result.end(); ++read_head )
-		{
-			CCON( !function( *read_head ) );
-			if( read_head != write_head )
-			{
-				*write_head = *read_head;
-			}
-
-			++write_head;
-		}
-
-		if( write_head != result.end() )
-		{
-			result.erase( write_head, result.end() );
-		}
-
-		return result;
-	};
 }
 }
 }

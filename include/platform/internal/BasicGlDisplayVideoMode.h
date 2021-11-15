8975dcd35d54378ca63bc9bc822c72c2531569a7
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 18:26:46 +0300

    Functions will be moved to public type.

--- include/platform/internal/BasicGlDisplayVideoMode.h
+++ include/platform/internal/BasicGlDisplayVideoMode.h
@@ -21,30 +21,6 @@ namespace Internal
 	{
 	// Public interface.
 	public:
-		// Get the width of resolution.
-		inline const size32_t GetWidth() const			{ return m_width; };
-
-		// Get the height of resolution.
-		inline const size32_t GetHeight() const			{ return m_height; };
-
-		// Get the bit rate.
-		inline const size32_t GetBitRate() const		{ return m_bit_rate; };
-
-		// Get the refresh rate.
-		inline const size32_t GetRefreshRate() const	{ return m_refresh_rate; };
-
-		// Get the aspect ratio, which represents `width / height` proportion.
-		inline const float GetAspectRatio() const		{ return m_aspect; };
-
-
-		// Whether the video mode is suitable for purposes of OpenGL.
-		inline const bool IsOpenGlCompatible() const	{ return m_is_compatible; };
-
-		// Whether the video mode may be used as full-screen display mode.
-		inline const bool HasFullscreenSupport() const	{ return m_allow_fullscreen; };
-
-		// Whether the video mode may be used for window while display in windowed mode.
-		inline const bool HasWindowSupport() const		{ return m_allow_windowed; };
 
 	// Heirs construction and initialization.
 	protected:

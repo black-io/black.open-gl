d548149c3ad691fd3f399b1bacc9e14ea60491c5
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 19:27:20 +0300

    Basic EGL connection refactored.

--- include/platform/internal/BasicEglConnection.h
+++ include/platform/internal/BasicEglConnection.h
@@ -10,21 +10,17 @@ inline namespace Platform
 namespace Internal
 {
 	/**
-		@brief	Basic implementation of GPU connection.
+		@brief	Basic interface of EGL-compatible connection.
 
-		RHI connection should implement the functionality to communicate with current hardware setup.
-		All other RHI types may be used only with support of RHI connection.
-
-		This type implements the first layer of platform-agnostic RHI connection type.
+		This type implements the first layer of platform-agnostic EGL Connection type.
 		This type is strictly internal and should never been used outside of subsystem.
 	*/
-	class BasicGlRhiConnection : private Black::NonTransferable
+	class BasicEglConnection : private Black::NonTransferable
 	{
 	// Heirs construction and initialization.
 	protected:
-		BasicGlRhiConnection()								= default;
-		BasicGlRhiConnection( const BasicGlRhiConnection& )	= default;
-		~BasicGlRhiConnection()								= default;
+		BasicEglConnection()	= default;
+		~BasicEglConnection()	= default;
 	};
 }
 }

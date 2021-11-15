0e1018e648a2a7265055ecb812e8f618e0e7ebe3
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 23:33:56 +0300

    Logging channel included.

--- source/platform/GlDisplayVideoMode.cpp
+++ source/platform/GlDisplayVideoMode.cpp
@@ -7,6 +7,13 @@ inline namespace OpenGl
 {
 inline namespace Platform
 {
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/OpenGL/Video Mode";
+}
+
+
 	GlDisplayVideoMode::GlDisplayVideoMode( const ConstructionInfo& info )
 		: PlatformSpecific::GlDisplayVideoMode<Black::BUILD_PLATFORM>{ info }
 	{

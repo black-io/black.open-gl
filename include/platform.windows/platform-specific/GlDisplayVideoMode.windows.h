92234fe822d1b30c641dd1973ab2ec74257c6ce6
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 17:35:37 +0300

    Display video mode refactored for Windows Desktop platform.

--- include/platform.windows/platform-specific/GlDisplayVideoMode.windows.h
+++ include/platform.windows/platform-specific/GlDisplayVideoMode.windows.h
@@ -10,21 +10,21 @@ inline namespace Platform
 namespace PlatformSpecific
 {
 	/**
-		@brief	Platform dependent display video mode implementation.
+		@brief	Platform-specific implementation of Display Video mode in term of Windows Desktop platform.
 
-		On Windows Desktop platform the RHI to display video mode is implemented via DXGI library.
-		This type carries only the description and the most basic metrics of display video mode.
+		On Windows Desktop platform the Video Mode is implemented via DXGI library.
+		This type carries only the description and the most basic metrics.
 
-		Implements the second layer of platform-agnostic display video mode type.
+		Implements the second layer of platform-agnostic Display Video Mode type.
 		This type is strictly internal and should never been used outside of subsystem.
 	*/
 	template<>
-	class GlRhiVideoMode<Black::PlatformType::WindowsDesktop> : public Internal::BasicGlRhiVideoMode
+	class GlDisplayVideoMode<Black::PlatformType::WindowsDesktop> : public Internal::BasicGlDisplayVideoMode
 	{
 	// Public inner types.
 	public:
 		// Information for instance construction.
-		using ConstructionInfo = Internal::RhiVideoModeInformationConsumer::VideoModeInfo;
+		using ConstructionInfo = Internal::DisplayVideoModeInformationConsumer::VideoModeInfo;
 
 	// Platform-specific interface.
 	public:
@@ -36,14 +36,14 @@ namespace PlatformSpecific
 
 	// Heirs construction and initialization.
 	protected:
-		using BasicGlRhiVideoMode::BasicGlRhiVideoMode;
-		explicit GlRhiVideoMode( const ConstructionInfo& info );
-		~GlRhiVideoMode() = default;
+		using BasicGlDisplayVideoMode::BasicGlDisplayVideoMode;
+		explicit GlDisplayVideoMode( const ConstructionInfo& info );
+		~GlDisplayVideoMode() = default;
 
 	// Heirs interface.
 	protected:
 		// Swap the state with given instance.
-		void Swap( GlRhiVideoMode<Black::PlatformType::WindowsDesktop>& other );
+		void Swap( GlDisplayVideoMode<Black::PlatformType::WindowsDesktop>& other );
 
 	// Private state.
 	private:
--- source/platform.windows/platform-specific/GlDisplayVideoMode.windows.cpp
+++ source/platform.windows/platform-specific/GlDisplayVideoMode.windows.cpp
@@ -12,29 +12,29 @@ namespace PlatformSpecific
 namespace
 {
 	// Logging channel.
-	constexpr const char* LOG_CHANNEL = "Black/OpenGL/RHI Video Mode";
+	constexpr const char* LOG_CHANNEL = "Black/OpenGL/Video Mode";
 }
 
 
-	GlRhiVideoMode<Black::PlatformType::WindowsDesktop>::GlRhiVideoMode( const ConstructionInfo& info )
+	GlDisplayVideoMode<Black::PlatformType::WindowsDesktop>::GlDisplayVideoMode( const ConstructionInfo& info )
 		: m_mode_desc{ std::get<0>( info ) }
 	{
 		m_width			= m_mode_desc.Width;
 		m_height		= m_mode_desc.Height;
-		m_bit_rate		= Black::GlRhiConnection::GetDisplayFormatBitrate( m_mode_desc.Format );
+		//m_bit_rate		= Black::GlRhiConnection::GetDisplayFormatBitrate( m_mode_desc.Format );
 		m_refresh_rate	= m_mode_desc.RefreshRate.Numerator / std::max( m_mode_desc.RefreshRate.Denominator, 1U );
 		m_aspect		= m_width / float( m_height );
 
-		m_is_compatible		= Black::GlRhiConnection::IsDisplayFormatCompatible( m_mode_desc.Format );
+		//m_is_compatible		= Black::GlRhiConnection::IsDisplayFormatCompatible( m_mode_desc.Format );
 		m_allow_fullscreen	= true;
 		m_allow_windowed	= true;
 	}
 
-	void GlRhiVideoMode<Black::PlatformType::WindowsDesktop>::Swap( GlRhiVideoMode<Black::PlatformType::WindowsDesktop>& other )
+	void GlDisplayVideoMode<Black::PlatformType::WindowsDesktop>::Swap( GlDisplayVideoMode<Black::PlatformType::WindowsDesktop>& other )
 	{
 		using std::swap;
 
-		BasicGlRhiVideoMode::Swap( other );
+		BasicGlDisplayVideoMode::Swap( other );
 		swap( m_mode_desc, other.m_mode_desc );
 	}
 }

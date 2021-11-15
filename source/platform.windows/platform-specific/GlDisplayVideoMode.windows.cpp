697ee2735c008b2a160cacc7f3734229125b72f2
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 23:34:54 +0300

    New functions used to get bit-rate of video mode.

--- source/platform.windows/platform-specific/GlDisplayVideoMode.windows.cpp
+++ source/platform.windows/platform-specific/GlDisplayVideoMode.windows.cpp
@@ -1,6 +1,9 @@
 #include <black/open-gl.h>
 
 
+#include "dxgi/functions.dxgi-video-modes.h"
+
+
 namespace Black
 {
 inline namespace OpenGl
@@ -21,11 +24,11 @@ namespace
 	{
 		m_width			= m_mode_desc.Width;
 		m_height		= m_mode_desc.Height;
-		//m_bit_rate		= Black::GlRhiConnection::GetDisplayFormatBitrate( m_mode_desc.Format );
+		m_bit_rate		= GetDisplayFormatBitrate( m_mode_desc.Format );
 		m_refresh_rate	= m_mode_desc.RefreshRate.Numerator / std::max( m_mode_desc.RefreshRate.Denominator, 1U );
 		m_aspect		= m_width / float( m_height );
 
-		//m_is_compatible		= Black::GlRhiConnection::IsDisplayFormatCompatible( m_mode_desc.Format );
+		m_is_compatible		= IsDisplayFormatCompatible( m_mode_desc.Format );
 		m_allow_fullscreen	= true;
 		m_allow_windowed	= true;
 	}

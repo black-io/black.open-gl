33d5bd2c694c2a42c64f963e0c155ab19b674028
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 23:33:36 +0300

    Logging channel included.

--- source/platform/GlDisplayHandle.cpp
+++ source/platform/GlDisplayHandle.cpp
@@ -7,6 +7,13 @@ inline namespace OpenGl
 {
 inline namespace Platform
 {
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/OpenGL/Display Handle";
+}
+
+
 	GlDisplayHandle::GlDisplayHandle( const ConstructionInfo& info )
 		: PlatformSpecific::GlDisplayHandle<Black::BUILD_PLATFORM>{ info }
 	{

8d6ac1fc42835947dd063d0d5ff3df99862fe975
Frank Stein <dr.frank.stain@gmail.com>
2021-11-15 23:32:49 +0300

    Logging channel included.

--- source/platform/EglConfiguration.cpp
+++ source/platform/EglConfiguration.cpp
@@ -7,6 +7,13 @@ inline namespace OpenGl
 {
 inline namespace Platform
 {
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/OpenGL/EGL Configuration";
+}
+
+
 	EglConfiguration::EglConfiguration( const ConstructionInfo& info )
 		: PlatformSpecific::EglConfiguration<Black::BUILD_PLATFORM>{ info }
 	{

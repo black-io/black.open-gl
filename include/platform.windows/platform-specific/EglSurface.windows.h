1da389cf7a4f9c07650e58e4347399076b355f32
Frank Stein <dr.frank.stain@gmail.com>
2021-11-16 19:43:37 +0300

    Implementation of EGL Surface in terms of Windows Dsktop platform.

--- include/platform.windows/platform-specific/EglSurface.windows.h
+++ include/platform.windows/platform-specific/EglSurface.windows.h
@@ -21,20 +21,60 @@ namespace PlatformSpecific
 	template<>
 	class EglSurface<Black::PlatformType::WindowsDesktop> : public Internal::BasicEglSurface
 	{
+	// Inner public types.
+	public:
+		// Type of surface.
+		using SurfaceType = Internal::SurfaceType;
+
 	// Platform-specific interface.
 	public:
+		// Connect the surface to given window.
+		const bool ConnectWindow( const Black::EglDisplay& display, const Black::EglConfiguration& configuration, const ::HWND window_handle );
+
+		// Connect the surface to pixel buffer.
+		const bool ConnectPixelBuffer(
+			const Black::EglDisplay& display,
+			const Black::EglConfiguration& configuration,
+			const size32_t width,
+			const size32_t height
+		);
 
 	// Platform-agnostic interface.
 	public:
+		// Perform the surface disconnection.
+		void Disconnect();
+
+
+		// Whether the surface object is connected.
+		inline const bool IsConnected() const		{ return m_surface_context != nullptr; };
+
+		// Get the type of surface.
+		inline const SurfaceType GetType() const	{ return m_type; };
 
 	// Heirs construction and initialization.
 	protected:
-		EglSurface() = default;
-		~EglSurface();
+		EglSurface()	= default;
+		~EglSurface()	= default;
+
+	// Private types.
+	private:
+		// Internal state composition.
+		using SurfaceState = std::variant<
+			std::monostate,						// Type of surface is undefined.
+			Internal::WindowSurfaceState,		// For window surfaces.
+			Internal::PixelBufferSurfaceState	// For pixel buffer surfaces.
+		>;
+
+	// Private interface.
+	private:
+		// Destroy the owned pixel buffer surface.
+		void DestroyPixelBuffer();
 
 	// Private state.
 	private:
-
+		::HDC			m_surface_context	= nullptr;					// Usable surface context.
+		SurfaceState	m_state				{ std::monostate{} };		// Composition of internal state.
+		SurfaceType		m_type				= SurfaceType::Undefined;	// Type of surface connection.
 	};
 }
 }
--- source/platform.windows/platform-specific/EglSurface.windows.cpp
+++ source/platform.windows/platform-specific/EglSurface.windows.cpp
@@ -1,5 +1,9 @@
 #include <black/open-gl.h>
 
+#include "wgl/bindings.wgl.h"
+#include "wgl/bindings.wgl-extensions.h"
+#include "wgl/functions.settings.h"
+
 
 namespace Black
 {
@@ -9,7 +13,177 @@ inline namespace Platform
 {
 namespace PlatformSpecific
 {
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/OpenGL/EGL Surface";
+}
 
+
+	const bool EglSurface<Black::PlatformType::WindowsDesktop>::ConnectWindow(
+		const Black::EglDisplay& display,
+		const Black::EglConfiguration& configuration,
+		const ::HWND window_handle
+	)
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting the surface to window." );
+
+		CRETE( IsConnected(), false, LOG_CHANNEL, "The surface is already connected." );
+		CRETE( ( configuration.GetDescription().dwFlags & PFD_DRAW_TO_WINDOW ) == 0, false, LOG_CHANNEL, "Configuration does not support windows." );
+
+		m_type		= SurfaceType::Window;
+		auto& state	= m_state.emplace<Internal::WindowSurfaceState>();
+
+		state.host_window	= window_handle;
+		m_surface_context	= ::GetDC( state.host_window );
+
+		if( ::SetPixelFormat( m_surface_context, configuration.GetIndex(), &configuration.GetDescription() ) != TRUE )
+		{
+			BLACK_LOG_DEBUG( LOG_CHANNEL, "Failed to set pixel format for window surface, error: 0x{:08X}.", ::GetLastError() );
+		}
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Surface successfully connected to window." );
+		return true;
+	}
+
+	const bool EglSurface<Black::PlatformType::WindowsDesktop>::ConnectPixelBuffer(
+		const Black::EglDisplay& display,
+		const Black::EglConfiguration& configuration,
+		const size32_t width,
+		const size32_t height
+	)
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting the surface to pixel buffer." );
+
+		CRETE( IsConnected(), false, LOG_CHANNEL, "The surface is already connected." );
+		CRETE( !configuration.GetPixelBufferSettings().is_supported, false, LOG_CHANNEL, "Given configuration does not support pixel buffers." );
+		CRETE( int32_t( width ) > configuration.GetPixelBufferSettings().maximum_width, false, LOG_CHANNEL, "Width limit exceeded." );
+		CRETE( int32_t( height ) > configuration.GetPixelBufferSettings().maximum_height, false, LOG_CHANNEL, "Height limit exceeded." );
+
+		const ::Wgl::ExtensionsState& extensions = ::Wgl::GetExtensionsState();
+		if( extensions.has_wgl_ext_pbuffer )
+		{
+			::Wgl::HPBUFFEREXT buffer_handle = ::Wgl::create_pbuffer_ext( display.GetDeviceContext(), configuration.GetIndex(), width, height, nullptr );
+			CRETE( buffer_handle == nullptr, false, LOG_CHANNEL, "Failed to create the pixel buffer, error: 0x{:08X}.", ::GetLastError() );
+
+			// Guard to destroy the pixel buffer object on any error.
+			auto buffer_handle_guard = Black::ScopeLeaveHandler{
+				[buffer_handle]()
+				{
+					if( ::Wgl::destroy_pbuffer_ext( buffer_handle ) != TRUE )
+					{
+						BLACK_LOG_WARNING( LOG_CHANNEL, "Destruction of pixel buffer goes wrong, error: 0x{:08X}.", ::GetLastError() );
+					}
+				}
+			};
+
+			const ::HDC surface_context = ::Wgl::get_pbuffer_dc_ext( buffer_handle );
+			CRETE( surface_context == nullptr, false, LOG_CHANNEL, "Failed to get device context for pixel buffer, error: 0x{:08X}.", ::GetLastError() );
+
+			auto& state = m_state.emplace<Internal::PixelBufferSurfaceState>();
+
+			buffer_handle_guard.Cancel();
+			m_type						= SurfaceType::PixelBuffer;
+			state.owned_pixel_buffer	= buffer_handle;
+			m_surface_context			= surface_context;
+		}
+		else if( extensions.has_wgl_arb_pbuffer )
+		{
+			::Wgl::HPBUFFERARB buffer_handle = ::Wgl::create_pbuffer_arb( display.GetDeviceContext(), configuration.GetIndex(), width, height, nullptr );
+			CRETE( buffer_handle == nullptr, false, LOG_CHANNEL, "Failed to create the pixel buffer, error: 0x{:08X}.", ::GetLastError() );
+
+			// Guard to destroy the pixel buffer object on any error.
+			auto buffer_handle_guard = Black::ScopeLeaveHandler{
+				[buffer_handle]()
+				{
+					if( ::Wgl::destroy_pbuffer_arb( buffer_handle ) != TRUE )
+					{
+						BLACK_LOG_WARNING( LOG_CHANNEL, "Destruction of pixel buffer goes wrong, error: 0x{:08X}.", ::GetLastError() );
+					}
+				}
+			};
+
+			const ::HDC surface_context = ::Wgl::get_pbuffer_dc_arb( buffer_handle );
+			CRETE( surface_context == nullptr, false, LOG_CHANNEL, "Failed to get device context for pixel buffer, error: 0x{:08X}.", ::GetLastError() );
+
+			auto& state = m_state.emplace<Internal::PixelBufferSurfaceState>();
+
+			buffer_handle_guard.Cancel();
+			m_type						= SurfaceType::PixelBuffer;
+			state.owned_pixel_buffer	= buffer_handle;
+			m_surface_context			= surface_context;
+		}
+		else
+		{
+			BLACK_LOG_ERROR( LOG_CHANNEL, "Pixel buffers are not supported by selected hardware." );
+			return false;
+		}
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Surface successfully connected to pixel buffer." );
+		return true;
+	}
+
+	void EglSurface<Black::PlatformType::WindowsDesktop>::Disconnect()
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Surface will be disconnected." );
+		switch( m_type )
+		{
+		case SurfaceType::Window:
+			if( ::ReleaseDC( std::get<Internal::WindowSurfaceState>( m_state ).host_window, m_surface_context ) != TRUE )
+			{
+				BLACK_LOG_WARNING( LOG_CHANNEL, "Device context was not released properly on surface disconnection." );
+			}
+			break;
+		case SurfaceType::PixelBuffer:
+			DestroyPixelBuffer();
+			break;
+		default:
+			break;
+		}
+
+		m_surface_context = nullptr;
+		m_state.emplace<std::monostate>();
+		m_type = SurfaceType::Undefined;
+	}
+
+	void EglSurface<Black::PlatformType::WindowsDesktop>::DestroyPixelBuffer()
+	{
+		const ::Wgl::ExtensionsState& extensions = ::Wgl::GetExtensionsState();
+
+		auto& state = std::get<Internal::PixelBufferSurfaceState>( m_state );
+		if( extensions.has_wgl_ext_pbuffer )
+		{
+			::Wgl::HPBUFFEREXT buffer_handle = (::Wgl::HPBUFFEREXT)state.owned_pixel_buffer;
+
+			if( ::Wgl::release_pbuffer_dc_ext( buffer_handle, m_surface_context ) != TRUE )
+			{
+				BLACK_LOG_WARNING( LOG_CHANNEL, "Failed to release device context of pixel buffer, error: 0x{:08X}.", ::GetLastError() );
+			}
+
+			if( ::Wgl::destroy_pbuffer_ext( buffer_handle ) != TRUE )
+			{
+				BLACK_LOG_WARNING( LOG_CHANNEL, "Destruction of pixel buffer goes wrong, error: 0x{:08X}.", ::GetLastError() );
+			}
+		}
+		else if( extensions.has_wgl_arb_pbuffer )
+		{
+			::Wgl::HPBUFFERARB buffer_handle = (::Wgl::HPBUFFERARB)state.owned_pixel_buffer;
+
+			if( ::Wgl::release_pbuffer_dc_arb( buffer_handle, m_surface_context ) != TRUE )
+			{
+				BLACK_LOG_WARNING( LOG_CHANNEL, "Failed to release device context of pixel buffer, error: 0x{:08X}.", ::GetLastError() );
+			}
+
+			if( ::Wgl::destroy_pbuffer_arb( buffer_handle ) != TRUE )
+			{
+				BLACK_LOG_WARNING( LOG_CHANNEL, "Destruction of pixel buffer goes wrong, error: 0x{:08X}.", ::GetLastError() );
+			}
+		}
+		else
+		{
+			BLACK_LOG_ERROR( LOG_CHANNEL, "Extensions was changed improperly. Pixel buffers now does not supported." );
+		}
+	}
 }
 }
 }

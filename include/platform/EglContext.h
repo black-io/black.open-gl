50b4db225f9ab67c77033f2a402b4c112e2ef580
Frank Stein <dr.frank.stain@gmail.com>
2021-11-17 19:24:13 +0300

    EGL context implemented.

--- include/platform/EglContext.h
+++ include/platform/EglContext.h
@@ -22,9 +22,57 @@ inline namespace Platform
 
 	// Contracts of public API.
 	public:
+		/// @see	EglContext::GetThreadId
+		using PlatformSpecific::EglContext<Black::BUILD_PLATFORM>::GetThreadId;
+
+		/// @see	EglContext::IsCurrent
+		using PlatformSpecific::EglContext<Black::BUILD_PLATFORM>::IsCurrent;
+
+		/// @see	EglContext::IsCurrentForThisThread
+		using PlatformSpecific::EglContext<Black::BUILD_PLATFORM>::IsCurrentForThisThread;
+
+		/// @see	EglContext::Finalize
+		using PlatformSpecific::EglContext<Black::BUILD_PLATFORM>::Finalize;
+
+		/// @see	EglContext::MakeCurrent
+		using PlatformSpecific::EglContext<Black::BUILD_PLATFORM>::MakeCurrent;
+
+		/// @see	EglContext::IsConnected
+		using PlatformSpecific::EglContext<Black::BUILD_PLATFORM>::IsConnected;
 
 	// Public interface.
 	public:
+		// Use the settings of given context.
+		const bool UseSettings( const EglContext& other );
+
+
+		// Set the context version. The function may be used only before connection.
+		const bool SetVersion( const int32_t major_version, const int32_t minor_version );
+
+		// Select the API profile. The function may be used only before connection.
+		const bool SelectApiProfile( const ApiProfile profile );
+
+		// Set the context debugging. The function may be used only before connection.
+		const bool SetDebugging( const bool should_debug );
+
+		// Allow the context to be connected with ahead version that requested. The function may be used only before connection.
+		const bool SetForwardVersiosCompatible( const bool allow_forward_versions );
+
+
+		// Get the major version of context.
+		inline const int32_t GetMajorVersion() const		{ return m_major_version; };
+
+		// Get the minor version of context.
+		inline const int32_t SetMinorVersion() const		{ return m_minor_version; };
+
+		// Get selected API profile.
+		inline const ApiProfile GetApiProfile() const		{ return m_api_profile; };
+
+		// Whether the context is debuggable.
+		inline const bool IsDebuggable() const				{ return m_is_debugable; };
+
+		// Whether the context is allowed to be created with any ahead version than requested.
+		inline const bool AllowForwardVersions() const		{ return m_is_forward_compatible; };
 	};
 }
 }
--- source/platform/EglContext.cpp
+++ source/platform/EglContext.cpp
@@ -16,7 +16,52 @@ namespace
 
 	EglContext::~EglContext()
 	{
+		Finalize();
+	}
 
+	const bool EglContext::UseSettings( const EglContext& other )
+	{
+		CRET( IsConnected(), false );
+
+		SetVersion( other.m_major_version, other.m_minor_version );
+		SelectApiProfile( other.m_api_profile );
+		SetDebugging( other.m_is_debugable );
+		SetForwardVersiosCompatible( other.m_is_forward_compatible );
+
+		return true;
+	}
+
+	const bool EglContext::SetVersion( const int32_t major_version, const int32_t minor_version )
+	{
+		CRET( IsConnected(), false );
+
+		m_major_version	= major_version;
+		m_minor_version	= minor_version;
+		return true;
+	}
+
+	const bool EglContext::SelectApiProfile( const ApiProfile profile )
+	{
+		CRET( IsConnected(), false );
+
+		m_api_profile = profile;
+		return true;
+	}
+
+	const bool EglContext::SetDebugging( const bool should_debug )
+	{
+		CRET( IsConnected(), false );
+
+		m_is_debugable = should_debug;
+		return true;
+	}
+
+	const bool EglContext::SetForwardVersiosCompatible( const bool allow_forward_versions )
+	{
+		CRET( IsConnected(), false );
+
+		m_is_forward_compatible = allow_forward_versions;
+		return true;
 	}
 }
 }

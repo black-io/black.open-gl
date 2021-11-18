4044673f6c9574e106f3cc39931b84cbbc8cb6de
Frank Stein <dr.frank.stain@gmail.com>
2021-11-18 22:42:04 +0300

    First code for initialization of OpenGL ES 3.0 subsystem.

---
+++ include/gl-es-3.0/functions.gl-es-3.0-initialization.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace GlEs30
+{
+	/**
+		@brief	Ensure the OpenGL ES 3.0 subsystem initialized and ready to be used.
+
+		Initialization is required since the OpenGL functions are not linked statically.
+		Due to process of initialization the subsystem loads the core profile of OpenGL interface, making possible to work with only certain version of OpenGL.
+
+		This function never fails, does not throw the errors and always succeeded. After the function is called it is true that the OpenGL functions may be used.
+		But once the function failed, the entire process will be terminated.
+
+		@param	connection	Instance of RHI connection to perform the possible initialization.
+	*/
+	void GlEnsureInitialized( Black::EglConnection& connection );
+}
+}
+}
---
+++ source/gl-es-3.0/functions.gl-es-3.0-initialization.cpp
@@ -0,0 +1,16 @@
+#include <black/open-gl/gl-es-3.0.h>
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace GlEs30
+{
+	void GlEnsureInitialized( Black::EglConnection& connection )
+	{
+	}
+}
+}
+}
+

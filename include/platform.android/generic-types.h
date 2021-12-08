0fa335e0bf8be687dc19e828a9a90614eae43a7c
Frank Stein <dr.frank.stain@gmail.com>
2021-12-08 17:54:53 +0300

    Generic OpenGL types.

---
+++ include/platform.android/generic-types.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace OpenGl
+{
+inline namespace Platform
+{
+inline namespace Android
+{
+	/**
+		@brief	Handle.
+		Original type is `GLhandleARB`. Aliased type is `unsigned int` or `void *` on Apple platforms.
+	*/
+	using GlHandleArb = uint32_t;
+}
+}
+}
+}

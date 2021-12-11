abf8276db73f4ecc6dd89834e9485b3c95ebc7a5
Frank Stein <dr.frank.stain@gmail.com>
2021-12-11 19:07:15 +0300

    First code for EGL surface implementation in terms of Android platform.

--- include/platform.android/platform-specific/EglSurface.android.h
+++ include/platform.android/platform-specific/EglSurface.android.h
@@ -9,7 +9,56 @@ inline namespace Platform
 {
 namespace PlatformSpecific
 {
+	/**
+		@brief	Platform-specific implementation of EGL-compatible surface in terms of Android platform.
 
+		This implementation directly uses the EGL API. It represents the `EGLSurface` type.
+
+		Implements the second layer of platform-agnostic EGL Surface type.
+		This type is strictly internal and should never been used outside of subsystem.
+	*/
+	template<>
+	class EglSurface<Black::PlatformType::Android> : public Internal::BasicEglSurface
+	{
+	// Platform-specific interface.
+	public:
+		// Connect the surface to given window.
+		const bool ConnectWindow( const Black::EglDisplay& display, const Black::EglConfiguration& configuration, const ::Jni::Surface& window_surface );
+
+		// Connect the surface to pixel buffer.
+		const bool ConnectPixelBuffer(
+			const Black::EglDisplay& display,
+			const Black::EglConfiguration& configuration,
+			const size32_t width,
+			const size32_t height
+		);
+
+
+		// Get the handle of host EGL display for this surface.
+		inline const ::EGLDisplay GetDisplayHandle() const	{ return m_display_handle; };
+
+		// Get the handle of corresponded EGL surface.
+		inline const ::EGLSurface GetHandle() const			{ return m_handle; };
+
+	// Platform-agnostic interface.
+	public:
+		// Finalize the surface connection.
+		void Finalize();
+
+
+		// Whether the surface object is connected.
+		inline const bool IsConnected() const { return m_handle != EGL_NO_SURFACE; };
+
+	// Heirs construction and initialization.
+	protected:
+		EglSurface()	= default;
+		~EglSurface()	= default;
+
+	// Private state.
+	private:
+		::EGLDisplay	m_display_handle	= EGL_NO_DISPLAY;	// EGL display that hosts the surface.
+		::EGLSurface	m_handle			= EGL_NO_SURFACE;	// Corresponded EGL surface.
+	};
 }
 }
 }
--- source/platform.android/platform-specific/EglSurface.android.cpp
+++ source/platform.android/platform-specific/EglSurface.android.cpp
@@ -9,7 +9,53 @@ inline namespace Platform
 {
 namespace PlatformSpecific
 {
+namespace
+{
+	// Logging channel.
+	constexpr const char* LOG_CHANNEL = "Black/OpenGL/EGL Surface";
+}
 
+
+	const bool EglSurface<Black::PlatformType::Android>::ConnectWindow(
+		const Black::EglDisplay& display,
+		const Black::EglConfiguration& configuration,
+		const ::Jni::Surface& window_surface
+	)
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting the surface to window." );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Surface successfully connected to window." );
+		return true;
+	}
+
+	const bool EglSurface<Black::PlatformType::Android>::ConnectPixelBuffer(
+		const Black::EglDisplay& display,
+		const Black::EglConfiguration& configuration,
+		const size32_t width,
+		const size32_t height
+	)
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting the surface to pixel buffer." );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Surface successfully connected to pixel buffer." );
+		return true;
+	}
+
+	void EglSurface<Black::PlatformType::Android>::Finalize()
+	{
+		CRET( !IsConnected() );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Surface will be disconnected." );
+
+		const bool is_succeeded = ::eglDestroySurface( m_display_handle, m_handle ) == EGL_TRUE;
+		if( !is_succeeded )
+		{
+			BLACK_LOG_WARNING( LOG_CHANNEL, "Destruction of EGL surface goes wrong, error: 0x{:08X}.", ::eglGetError() );
+		}
+
+		m_handle			= EGL_NO_SURFACE;
+		m_display_handle	= EGL_NO_DISPLAY;
+	}
 }
 }
 }

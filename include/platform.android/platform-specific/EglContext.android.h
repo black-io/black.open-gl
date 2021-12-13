4825b626be4e492534c65e3d604b98d452fbee43
Frank Stein <dr.frank.stain@gmail.com>
2021-12-13 10:10:32 +0300

    First code of EGL context implementation in terms of Android platform.

--- include/platform.android/platform-specific/EglContext.android.h
+++ include/platform.android/platform-specific/EglContext.android.h
@@ -9,7 +9,64 @@ inline namespace Platform
 {
 namespace PlatformSpecific
 {
+	/**
+		@brief	Platform-specific implementation of EGL-compatible context in terms of Android platform.
 
+		This implementation directly uses the EGL API. It represents the `EGLContext` type.
+
+		Implements the second layer of platform-agnostic EGL Context type.
+		This type is strictly internal and should never been used outside of subsystem.
+	*/
+	template<>
+	class EglContext<Black::PlatformType::Android> : public Internal::BasicEglContext
+	{
+	// Platform-specific interface.
+	public:
+		// Connect the context with given display.
+		const bool Connect( const Black::EglDisplay& display, const Black::EglConfiguration& configuration );
+
+		// Connect the context with given display and given host context.
+		const bool Connect( const Black::EglDisplay& display, const Black::EglConfiguration& configuration, const Black::EglContext& host_context );
+
+
+		// Get the handle of host EGL display for this context.
+		inline const ::EGLDisplay GetDisplayHandle() const	{ return m_display_handle; };
+
+		// Get the handle of corresponded EGL context.
+		inline const ::EGLContext GetHandle() const			{ return m_handle; };
+
+	// Platform-agnostic interface.
+	public:
+		// Finalize the context connection.
+		void Finalize();
+
+
+		// Make the context to be no more current for current thread.
+		const bool ResetCurrent( const Black::EglDisplay& display );
+
+		// Make the context to be current for current thread with given target surface.
+		const bool MakeCurrent( const Black::EglDisplay& display, const Black::EglSurface& target_surface );
+
+		// Make the context to be current for current thread with given target surfaces.
+		const bool MakeCurrent( const Black::EglDisplay& display, const Black::EglSurface& rendering_surface, const Black::EglSurface& reading_surface );
+
+
+		// Whether the context is connected.
+		inline const bool IsConnected() const	{ return m_handle != nullptr; };
+
+	// Heirs construction and initialization.
+	protected:
+		EglContext()	= default;
+		~EglContext()	= default;
+
+	// Private interface.
+	private:
+
+	// Private state.
+	private:
+		::EGLDisplay	m_display_handle	= EGL_NO_DISPLAY;
+		::EGLContext	m_handle			= EGL_NO_CONTEXT;
+	};
 }
 }
 }
--- source/platform.android/platform-specific/EglContext.android.cpp
+++ source/platform.android/platform-specific/EglContext.android.cpp
@@ -16,7 +16,83 @@ namespace
 }
 
 
+	const bool EglContext<Black::PlatformType::Android>::Connect( const Black::EglDisplay& display, const Black::EglConfiguration& configuration )
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting the context with given display." );
 
+		CRETE( IsConnected(), false, LOG_CHANNEL, "Context is already connected." );
+		CRETE( !display.IsConnected(), false, LOG_CHANNEL, "Display should be already connected." );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Context successfully connected with given display." );
+		return true;
+	}
+
+	const bool EglContext<Black::PlatformType::Android>::Connect(
+		const Black::EglDisplay& display,
+		const Black::EglConfiguration& configuration,
+		const Black::EglContext& host_context
+	)
+	{
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Connecting the context with given display and host context." );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Context successfully connected with given display and host context." );
+		return true;
+	}
+
+	void EglContext<Black::PlatformType::Android>::Finalize()
+	{
+		CRET( !IsConnected() );
+
+		EXPECTS( !IsCurrent() );
+
+		BLACK_LOG_DEBUG( LOG_CHANNEL, "Context will be finalized." );
+		if( ::eglDestroyContext( m_display_handle, m_handle ) == EGL_FALSE )
+		{
+			BLACK_LOG_WARNING( LOG_CHANNEL, "Destruction of EGL context goes wrong, error: 0x{:08X}.", ::eglGetError() );
+		}
+
+		m_handle			= EGL_NO_CONTEXT;
+		m_display_handle	= EGL_NO_DISPLAY;
+	}
+
+	const bool EglContext<Black::PlatformType::Android>::ResetCurrent( const Black::EglDisplay& display )
+	{
+		CRETE( m_is_current && !IsCurrentForThisThread(), false, LOG_CHANNEL, "Context should be reset from thread where it is current." );
+		CRETE( !display.IsConnected(), false, LOG_CHANNEL, "Given display should be already connected." );
+
+		m_is_current		= false;
+		m_current_thread_id	= {};
+		return true;
+	}
+
+	const bool EglContext<Black::PlatformType::Android>::MakeCurrent( const Black::EglDisplay& display, const Black::EglSurface& target_surface )
+	{
+		CRET( IsCurrentForThisThread(), true );
+		CRETE( m_is_current, false, LOG_CHANNEL, "Context already set as current in a different thread." );
+		CRETE( !display.IsConnected(), false, LOG_CHANNEL, "Given display should be already connected." );
+		CRETE( !target_surface.IsConnected(), false, LOG_CHANNEL, "Target surface should be already connected." );
+
+		m_is_current		= true;
+		m_current_thread_id	= std::this_thread::get_id();
+		return true;
+	}
+
+	const bool EglContext<Black::PlatformType::Android>::MakeCurrent(
+		const Black::EglDisplay& display,
+		const Black::EglSurface& rendering_surface,
+		const Black::EglSurface& reading_surface
+	)
+	{
+		CRET( IsCurrentForThisThread(), true );
+		CRETE( m_is_current, false, LOG_CHANNEL, "Context already set as current in a different thread." );
+		CRETE( !display.IsConnected(), false, LOG_CHANNEL, "Given display should be already connected." );
+		CRETE( !rendering_surface.IsConnected(), false, LOG_CHANNEL, "Rendering surface should be already connected." );
+		CRETE( !reading_surface.IsConnected(), false, LOG_CHANNEL, "Reading surface should be already connected." );
+
+		m_is_current		= true;
+		m_current_thread_id	= std::this_thread::get_id();
+		return true;
+	}
 }
 }
 }

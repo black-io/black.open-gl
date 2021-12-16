5d96031a90fa05f7ba52ea626364fc106406eb56
Frank Stein <dr.frank.stain@gmail.com>
2021-12-16 14:14:57 +0300

    More arguments for video modes construction on Android platform.

--- include/platform.android/internal/connection-visitors.h
+++ include/platform.android/internal/connection-visitors.h
@@ -65,7 +65,7 @@ namespace Internal
 	// Public inner types.
 	public:
 		// Information of display video mode.
-		using VideoModeInfo = std::tuple<int32_t, int32_t, int32_t, int32_t, ::Jni::PixelFormatCode>;
+		using VideoModeInfo = std::tuple<int32_t, int32_t, int32_t, int32_t, ::Jni::PixelFormatCode, size32_t>;
 
 	// Public virtual interface.
 	public:

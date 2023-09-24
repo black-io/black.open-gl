6fe04f24c65ba73185e40b3cfc59ad011ba44465
Frank Stein <dr.frank.stain@gmail.com>
2023-09-24 11:34:07 +0300

    Platform specific code moved to EGL subsystem.

---
+++ include/.public/black/open-gl/egl.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../../.prologue.h"
+
+// EGL bindings.
+#include "../../../egl/egl.h"
+
+// The epilogue of library.
+#include "../../../.epilogue.h"
--- include/egl/platform/platform.h
+++ include/egl/platform/platform.h
@@ -48,8 +48,20 @@ namespace PlatformSpecific
 // Second layer of platform-agnostic types. Platform-specific declarations.
 #include "platform-specific/forwards.h"
 
-// Include the platform-specific code.
-#include BLACK_CURRENT_PLATFORM_HEADER
+// Select the implementation of platform-specific code.
+#if( BLACK_WINDOWS_DESKTOP_PLATFORM )
+	#include "../platform.windows/platform.windows.h"
+#elif( BLACK_MAC_OS_PLATFORM )
+	#include "../platform.macos/platform.macos.h"
+#elif( BLACK_LINUX_PLATFORM )
+	#include "../platform.linux/platform.linux.h"
+#elif( BLACK_ANDROID_PLATFORM )
+	#include "../platform.android/platform.android.h"
+#elif( BLACK_IOS_PLATFORM )
+	#include "../platform.ios/platform.ios.h"
+#else
+	#error Current platform is unspecified or not defined
+#endif
 
 // Third layer of platform-agnostic types. Public platform-agnostic definitions.
 #include "GlAdapterHandle.h"
